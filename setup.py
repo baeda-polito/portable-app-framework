568bcdb82ff277d6a2e20a0b9be76a55587141ba
RobertoChiosa <roberto.chiosa@gmail.com>
2024-01-17 18:32:57 +0100

    refactor(git): removed cached files

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
-        "Topic :: Software Development :: HVAC Systems",
+        "Topic :: Software Development",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
